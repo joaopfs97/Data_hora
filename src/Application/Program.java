ef21cf8896a9ba7b7cb45bc36d11c9f4d145ae6a
joaopfs97 <joaopfs201@gmail.com>
2022-08-16 17:08:55 -0300

    Convertendo Data-hora

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,12 +5,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Program {
 
     public static void main(String[] args) {
+        
+        /* //Instanciando Datetime
         Locale.setDefault(Locale.US);
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
@@ -30,7 +33,29 @@ public class Program {
         System.out.println("Data e hora no fuso GMT 0 a partir de uma String: " + d06);
         System.out.println("Data local com imput: " + d08);
         System.out.println("Data e hora local com imput: " + d09);
+        */
         
+        // Convertendo data-hora
+        LocalDate d04 = LocalDate.parse("2022-08-16");
+        LocalDateTime d05 = LocalDateTime.parse("2022-08-16T15:08:20");
+        Instant d06 = Instant.parse("2022-08-16T15:08:20Z");
+        
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+        
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05 = " + d05.format(fmt4));
+        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("d06 = " + fmt5.format(d06));
+        System.out.println("d06 = " + d06.toString());
         
     }
     
